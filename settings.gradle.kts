c4924b1f444a23595aec793e48e7c9df5119bd97
castledking <thecastledking@gmail.com>
2026-06-15 17:43:56 -0700

    fix: skip Fabric subproject on JitPack via -Pjitpack property

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,5 +13,8 @@ plugins {
 rootProject.name = "GriefPrevention3D"
 
 include("gp3d-core")
-include("fabric-1.21.11")
-project(":fabric-1.21.11").projectDir = file("platforms/fabric-1.21.11")
+
+if (providers.gradleProperty("jitpack").orElse("false").get() != "true") {
+    include("fabric-1.21.11")
+    project(":fabric-1.21.11").projectDir = file("platforms/fabric-1.21.11")
+}
